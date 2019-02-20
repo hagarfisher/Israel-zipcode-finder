803faef3906da9c9e98e637783db03aa5637c206
Hagar Fisher <hagarfisher@gmail.com>
2019-02-20 21:41:53 +0200

    content_script - creating ifrastructure

--- content_script.js
+++ content_script.js
@@ -1,18 +1,5 @@
-// you will see this log in console log of current tab in Chrome when the script is injected
-//console.log("content_script.js");
-
-chrome.runtime.onMessage.addListener(function (cmd, sendResponse) {
-    console.log("chrome.runtime.onMessage: " + cmd);
-    switch (cmd) {
-        case "getHtml":
-            // retrieve document HTML and send to popup.js
-            sendResponse({ title: document.title, url: window.location.href, html: document.documentElement.innerHTML });
-            break;
-        case "getHeadTitle":
-            // retrieve title HTML and send to popup.js
-            sendResponse(document.getElementById("billing:postcode").value);
-            break;
-        default:
-            sendResponse(null);
-    }
-});
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        //create a var that gets the city/address/house number first
+        //do this so you can access the mail services' api.
+    });
